d7783f0bc2d5ea51889549705da2b1e4b5dcfc66
AntonMaslik <maslik.fortech@gmail.com>
2024-10-28 17:09:30 +0300

    add user entity, dto

--- src/user/dto/user-create-request.dto/user-create-request.dto.ts
+++ src/user/dto/user-create-request.dto/user-create-request.dto.ts
@@ -1,1 +1,17 @@
-export class UserCreateRequestDto {}
+import { IsNotEmpty, IsEmail, MinLength, IsArray } from "class-validator"
+
+export class UserCreateRequestDto {
+    @IsNotEmpty()
+    name: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string
+
+    @IsNotEmpty()
+    @MinLength(6)
+    password: string
+
+    @IsArray()
+    workspaces: [number]
+}
