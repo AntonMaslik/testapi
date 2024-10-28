336741f1ca25dfb361f7fc2d4c461bbeb046543d
AntonMaslik <maslik.fortech@gmail.com>
2024-10-28 15:24:57 +0300

    add methods rest api task

--- src/task/task/task.controller.ts
+++ src/task/task/task.controller.ts
@@ -1,20 +1,46 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Body } from '@nestjs/common';
+import { TaskCreateRequestDto } from '../dto/task-create-request.dto/task-create-request.dto';
+import { TaskService } from './task.service';
+import { TaskEntity } from '../entity/task.entity/task.entity';
+import { TaskUpdateRequestDto } from '../dto/task-update-request.dto/task-update-request.dto';
+
 
 @Controller('task')
 export class TaskController {
+    constructor(private taskServices: TaskService){}
+
     @Post()
-    public createTask(): string {
-        return 'post';
+    public postTask(@Body() task:TaskCreateRequestDto): Promise<void> {
+        this.taskServices.createTask(task);
+        return new Promise<void>((resolve, reject) => {
+            resolve();
+        });
     }
 
     @Get()
-    public getTasks(): string {
-        return "get";
+    public getTask(@Body('uuid') uuid: string): Promise<TaskEntity> {
+        return this.taskServices.getTaskById(uuid);
+    }
+
+    @Get('/all')
+    public getAllTasks(): Promise<TaskEntity[]> {
+        return this.taskServices.getAllTasks();
     }
 
     @Delete()
-    public removeTask(id: number): string {
-        return "delete task"
+    public deleteTask(@Body('uuid') uuid: string): Promise<void> {
+        this.taskServices.deleteTask(uuid);
+        return new Promise<void>((resolve, reject) => {
+            resolve();
+        })
     }
-    
+
+    @Put()
+    public updateTask(@Body() task: TaskUpdateRequestDto): Promise<void> {
+       this.taskServices.updateTask(task);
+       return new Promise<void>((resolve, reject) => {
+            resolve();
+       }) 
+    }
+
 }
