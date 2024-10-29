4aff0f696e9e8015b1112612f3aa2a6c743b04b3
AntonMaslik <maslik.fortech@gmail.com>
2024-10-29 10:32:42 +0300

    create dto

--- src/auth/dto/sign-in-dto.ts
+++ src/auth/dto/sign-in-dto.ts
@@ -1,1 +1,13 @@
-export class SignInDto {}
+import { IsNotEmpty, IsEmail, IsString, MinLength } from "class-validator";
+
+export class SignInDto {
+
+    @IsNotEmpty()
+    @IsEmail({}, {message: 'Please enter correct email'})
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(6)
+    password: string;
+}
--- src/auth/dto/sign-up-dto.ts
+++ src/auth/dto/sign-up-dto.ts
@@ -1,1 +1,17 @@
-export class SignUpDto {}
+import { IsNotEmpty, IsString, IsEmail, MinLength } from "class-validator"
+
+export class SignUpDto {
+
+    @IsNotEmpty()
+    @IsString()
+    name: string
+
+    @IsNotEmpty()
+    @IsEmail({}, {message: "Please enter correct email"})
+    email: string
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(6, {message: "Password must be at least 6 characters long"})
+    password: string;
+}
