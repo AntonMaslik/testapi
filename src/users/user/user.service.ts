714d666d377a8056741474cedbf8e22d70127156
AntonMaslik <maslik.fortech@gmail.com>
2024-10-30 10:32:29 +0300

    refresh token add

--- src/users/user/user.service.ts
+++ src/users/user/user.service.ts
@@ -51,4 +51,14 @@ export class UserService {
         await this.usersRepository.softRemove(user);
         return user;
     }
+
+    async refreshToken(id: number, token: string){
+        let user = await this.usersRepository.findOne({
+            where: {
+                id: id
+            }
+        })
+        user.refreshToken = token
+        await this.usersRepository.save(user);
+    }  
 }
