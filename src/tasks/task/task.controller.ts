9c2343afe8d1b4fe1ed682a41157af2b4c353c16
AntonMaslik <maslik.fortech@gmail.com>
2024-10-30 14:23:09 +0300

    fix task controller

--- src/tasks/task/task.controller.ts
+++ src/tasks/task/task.controller.ts
@@ -3,30 +3,16 @@ import { TaskCreateRequestDto } from '../dto/task-create-request.dto';
 import { TaskService } from './task.service';
 import { TaskEntity } from '../entity/task.entity';
 import { TaskUpdateRequestDto } from '../dto/task-update-request.dto';
-<<<<<<< Updated upstream
-import { UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-
-=======
 import { UseGuards } from '@nestjs/common';
 import { RefreshTokenGuard } from 'src/auth/guards/refreshToken.guard';
->>>>>>> Stashed changes
+
 
 @Controller('tasks')
 export class TaskController {
     constructor(private taskServices: TaskService){}
 
-<<<<<<< Updated upstream
-    @UseGuards(AuthGuard('jwt'))
-=======
-<<<<<<< Updated upstream
-=======
+
     @UseGuards(RefreshTokenGuard)
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     @Post()
     postTask(@Body() task:TaskCreateRequestDto): Promise<void> {
         this.taskServices.createTask(task);
@@ -36,53 +22,28 @@ export class TaskController {
         });
     }
     
-<<<<<<< Updated upstream
-    @UseGuards(AuthGuard('jwt'))
-=======
-<<<<<<< Updated upstream
-=======
     @UseGuards(RefreshTokenGuard)
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     @Get('all')
     getAllTasks(): Promise<TaskEntity[]> {
         return this.taskServices.getAllTasks();
     }
 
-<<<<<<< Updated upstream
-    @UseGuards(AuthGuard('jwt'))
-=======
-<<<<<<< Updated upstream
-=======
+
     @UseGuards(RefreshTokenGuard)
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     @Get(':id')
     getTask(@Param('id', ParseIntPipe) id: number): Promise<TaskEntity> {
         return this.taskServices.getTaskById(id);
     }
 
-<<<<<<< Updated upstream
-    @UseGuards(AuthGuard('jwt'))
-=======
-<<<<<<< Updated upstream
-=======
+
     @UseGuards(RefreshTokenGuard)
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     @Delete(':id')
     deleteTask(@Param('id', ParseIntPipe) id: number){
         return this.taskServices.deleteTask(id);
     }
 
-<<<<<<< Updated upstream
-    @UseGuards(AuthGuard('jwt'))
-=======
-<<<<<<< Updated upstream
-=======
+
     @UseGuards(RefreshTokenGuard)
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     @Put(':id')
     updateTask(@Body() task: TaskUpdateRequestDto): Promise<TaskEntity> {
        return this.taskServices.updateTask(task);
