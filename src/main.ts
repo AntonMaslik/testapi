3e17486a97ff01db2bd87edcf1bd249ffd019779
AntonMaslik <maslik.fortech@gmail.com>
2024-10-31 12:06:23 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe({whitelist: true}))
+
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
