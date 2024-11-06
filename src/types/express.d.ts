812bfc0b702c971079be04477f1104236c4f4376
Anton <maslik.fortech@gmail.com>
2024-11-06 23:53:31 +0300

    types update

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,4 +1,4 @@
-import { RolesEntity } from 'src/auth/roles/roles.entity';
+import { RolesEntity } from 'src/auth/roles/entity/roles.entity';
 import { UserEntity } from 'src/users/entity/user.entity';
 
 declare global {
