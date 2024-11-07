7a7ee2fc78e2169908d433b62577ef3ec2bb82be
AntonMaslik <maslik.fortech@gmail.com>
2024-11-07 12:52:57 +0300

    add import

--- src/auth/roles/roles/roles.service.ts
+++ src/auth/roles/roles/roles.service.ts
@@ -8,6 +8,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from 'src/users/entity/user.entity';
 import { roleUpdateDto } from '../dto/role-update-dto';
+import { isAdmin } from '../helpers/helperIsAdmin';
 
 @Injectable()
 export class RolesService {
