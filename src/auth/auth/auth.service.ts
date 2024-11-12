2efa2fa0d5d437389f1087fafbbd8778cebb21cc
AntonMaslik <maslik.fortech@gmail.com>
2024-11-12 17:37:32 +0300

    Delete module init the method

--- src/auth/auth/auth.service.ts
+++ src/auth/auth/auth.service.ts
@@ -2,7 +2,6 @@ import {
     BadRequestException,
     Injectable,
     ForbiddenException,
-    OnModuleInit,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, UpdateResult } from 'typeorm';
