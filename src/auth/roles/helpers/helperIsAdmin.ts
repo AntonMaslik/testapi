ec22653209b09ba55c529d73b76cbaa7aa512bdb
AntonMaslik <maslik.fortech@gmail.com>
2024-11-15 15:34:23 +0300

    simple helper

--- src/auth/roles/helpers/helperIsAdmin.ts
+++ src/auth/roles/helpers/helperIsAdmin.ts
@@ -3,7 +3,5 @@ import { UserEntity } from 'src/users/entity/user.entity';
 import { Role } from '../roles/roles.enum';
 
 export async function isAdmin(user: UserEntity): Promise<boolean> {
-    const userRoles = user.roles.map((role) => role.name);
-
-    return userRoles.includes(Role.ADMIN);
+    return user.roles.some(({ name }) => name === Role.ADMIN);
 }
