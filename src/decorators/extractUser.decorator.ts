ea19074376b48abadedebc3c186c6bca7310455b
AntonMaslik <maslik.fortech@gmail.com>
2024-11-15 15:03:25 +0300

    fix

--- src/decorators/extractUser.decorator.ts
+++ src/decorators/extractUser.decorator.ts
@@ -4,8 +4,8 @@ import { Request } from 'express';
 
 export const ExtractUser = createParamDecorator(
     (data: unknown, ctx: ExecutionContext) => {
-        const { user }: Request = ctx.switchToHttp().getRequest();
+        const { userDb }: Request = ctx.switchToHttp().getRequest();
 
-        return user['userDb'];
+        return userDb;
     },
 );
